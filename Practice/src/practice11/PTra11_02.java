edd9b7ddaa5f975a5a9f2094523d399d24d4f205
MasashiSuyama <liike.hawks9503@gmail.com>
2018-04-04 14:45:28 +0900

    practice12 まで完了

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -5,16 +5,21 @@ package practice11;
  *------------------------------------------------------------
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
-
+import java.util.Scanner;
 public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+		Book[] a = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
-
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-
+		for(int i = 0 ; i < a.length ; i++) {
+			if(a[i].title.contains(name)) {
+				System.out.println(a[i].dispBookInfo());
+			}
+		}
 	}
 }
