0b96e19ac6f17dd4e4cbf9a7fd8f32222653c45e
MasashiSuyama <liike.hawks9503@gmail.com>
2018-04-09 14:09:18 +0900

    Ptra17_04まで完了

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -15,6 +15,15 @@ public class PTra17_04 {
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
-
+		try{
+			for(int i = 0 ; i < str.length() ;i++) {
+				System.out.print(str.charAt(i));
+				Thread.sleep(500);
+			}
+		} catch(InterruptedException s) {
+			System.out.println("例外が発生しました");
+		} catch(IllegalArgumentException s) {
+			System.out.println("例外が発生しました");
+		}
 	}
 }
